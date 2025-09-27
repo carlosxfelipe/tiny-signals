0a3efb787b1f36e8c505597792a4823253a4e38a
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-09-26 21:57:39 -0300

    Refactor Navbar component to improve structure and layout with a responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,23 @@
+// src/components/Navbar.tsx
 import { h } from "@tiny/tiny-signals.ts";
 import { StyleSheet } from "@styles/stylesheet.ts";
 
 export default function Navbar() {
-  return <header style={styles.navbar}>Navbar</header>;
+  return (
+    <header style={styles.navbar}>
+      <div style={styles.wrap}>
+        <div style={styles.brand}>Navbar</div>
+        <nav style={styles.links}>
+          <a class="link" href="#/">
+            Início
+          </a>
+          <a class="link" href="#/counter">
+            Contador
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -12,5 +27,19 @@ const styles = StyleSheet.create({
     "margin-bottom": "24px",
     background: "var(--btn-bg)",
     color: "var(--fg)",
+  },
+  wrap: {
+    display: "flex",
+    "align-items": "center",
+    "justify-content": "space-between",
+    width: "min(100%, 1024px)",
+    margin: "0 auto",
+  },
+  brand: {
+    "font-weight": 600,
+  },
+  links: {
+    display: "flex",
+    gap: "16px",
   },
 });
